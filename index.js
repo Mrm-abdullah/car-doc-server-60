f7608cbc09e70da3914174fe8ee975248a7add1d
mrmabdullah <mohammodraselmia128@gmail.com>
2024-05-09 10:03:37 +0600

    find data with, server

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ app.use(express.json());
 
 
 
-
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.okzu8ip.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -31,12 +30,20 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+        const servicesCollection = client.db("carDocs").collection("service");
+
+        app.get('/services', async(req, res) => {
+            const cursor = servicesCollection.find();
+            const result = await cursor.toArray();
+            res.send(result)
+        })
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
